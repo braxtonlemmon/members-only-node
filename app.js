a24cc6d1c877547c7bcb164fb0d1679a72b27eac
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-04-21 10:49:48 +0200

    Add mongoose and setup mongoose connection

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// Setup mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
